cd650bbd5503028b6f980a644c1a7fd7fafdffc0
reneejoo22 <renee.joo03@gmail.com>
2023-10-11 10:02:03 +0900

    초기화, 집합 함수 추가

--- Algorithm-Graph2.c
+++ Algorithm-Graph2.c
@@ -3,3 +3,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define True 1
+#define False 0
+
+#define MaxVertices 100
+#define INF 1000
+
+int parent[MaxVertices];	//부모노드
+
+void set_init(int n) {	//초기화
+	
+	for (int i = 0; i < n; i++)
+		parent[i] = -1;	//입력받은 값만큼 -1로 초기화
+}
+
+//curr 속하는 집합을 반환
+int set_find(int curr) {
+
+	if (parent[curr] == -1)	//입력받은 위치가 초기화된 상태면 반환
+		return curr;	
+	while (parent[curr] != -1)curr = parent[curr];	//아닐시 아닌 값 반환
+	return curr;
+}
+
+//두개의 원소가 속한 집합을 합침
+void set_union(int a, int b) {
+
+}
