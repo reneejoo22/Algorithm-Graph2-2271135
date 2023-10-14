46c7547d48c934ed694e5ea801a1da32c0644edc
reneejoo22 <renee.joo03@gmail.com>
2023-10-15 02:19:48 +0900

    주석추가

--- Algorithm-Graph2.c
+++ Algorithm-Graph2.c
@@ -24,11 +24,12 @@ int choose(int distance[], int n, int found[]) {
 	minpos = -1;
 
 	for (i = 0; i < n; i++)
-		if (distance[i] < min && !found[i]) {
+		if (distance[i] < min && !found[i]) {	//0의 정점과 이어진 다른 정점 중 가중치가 가장 작은 것
 			min = distance[i];
-			minpos = i;
+			minpos = i;	//몇번째 자리에 있는지를 저장하는 위치 값
 		}
-	return minpos;
+	//printf("\nminpos: %d\n", minpos);
+	return minpos;	//위치 값을 보냄
 }
 
 void print_status(GraphType* g) {
@@ -38,7 +39,7 @@ void print_status(GraphType* g) {
 	printf("distance: ");
 
 	for (int i = 0; i < g->n; i++) {
-		if (distance[i] == INF)
+		if (distance[i] == INF)	//값이 없는 경우 * 출력
 			printf(" * ");
 		else
 			printf("%2d ", distance[i]);
@@ -53,20 +54,24 @@ void print_status(GraphType* g) {
 void shortest_path(GraphType* g, int start) {
 
 	int i, u, w;
-	for (i = 0; i < g->n; i++) {	//초기화
-		distance[i] = g->weight[start][i];
-		found[i] = False;
-	}
-	found[start] = True;
-	distance[start] = 0;
 
-	for (i = 0; i < g->n - 1; i++) {
-		print_status(g);
-		u = choose(distance, g->n, found);
-		found[u] = True;
+	for (i = 0; i < g->n; i++) {	//정점0의 인접 행렬 값으로 초기화
+		distance[i] = g->weight[start][i];	//가중치	
+		found[i] = False;	//전부 0으로
+	}
+	found[start] = True;	// found:  1  0  0  0  0  0  0
+	distance[start] = 0;	// distance:  0  7  *  *  3 10  *
+
+	for (i = 0; i < g->n - 1; i++) {	//정점-1만큼, 6번
+		print_status(g);	//입력 된 found와 distance값 출력
+		u = choose(distance, g->n, found);	//첫번째 정점과 가장 가까운 정점의 위치 값 저장
+		found[u] = True;	//위치한 자리에 1 값을 넣음
+
+		
 		for (w = 0; w < g->n; w++) {
 			if (!found[w])
 				if (distance[u] + g->weight[u][w] < distance[w])
+					//가장 작은 정점 값과+ 그 정점의 가장 가까운 것들 찾기
 					distance[w] = distance[u] + g->weight[u][w];
 		}
 	}
